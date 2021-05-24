fc53e69fe4a00d46552b918f9d7370d34bb0fe56
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2021-05-24 14:58:51 +0200

    script para tirar los tests cualitativos

---
+++ test_qualitatius.py
@@ -0,0 +1,55 @@
+__authors__ = ['1354223', '1571136', '1563587']
+__group__ = 'DM.18'
+
+import numpy as np
+from  Kmeans import KMeans, get_colors
+from KNN import KNN
+from utils_data import read_dataset, visualize_k_means, visualize_retrieval, Plot3DCloud
+import matplotlib.pyplot as plt
+import cv2
+
+from my_labeling import Retrieval_by_color, Retrieval_by_shape, Retrieval_combined
+
+if __name__ == '__main__':
+    # Load all the images and GT
+    train_imgs, train_class_labels, train_color_labels, \
+    test_imgs, test_class_labels, test_color_labels = read_dataset(ROOT_FOLDER='./images/',
+                                                                   gt_json='./images/gt.json')
+
+    # List with all the existant classes
+    classes = list(set(list(train_class_labels) + list(test_class_labels)))
+
+    images_to_test = test_imgs[0:50]
+
+    # SETUP KMEANS
+    imgs = []
+    labels = []
+    for img in images_to_test:
+        kmeans = KMeans(img, 0)
+        kmeans.find_bestK(len(train_color_labels))
+        k = kmeans.K
+        kmeans = KMeans(img, k)
+        kmeans.fit()
+        imgs.append(img)
+        labels.append(get_colors(kmeans.centroids))
+
+    npimgs = np.array(imgs)
+    nplabels = np.array(labels, dtype=object)
+
+    # SETUP KNN
+    knn = KNN(train_imgs, train_class_labels)
+    class_labels = knn.predict(images_to_test, 2)
+
+    # RETRIEVE BY COLOR
+    found = Retrieval_by_color(npimgs, nplabels, ['Red'])
+    visualize_retrieval(found, len(found))
+
+
+    # RETRIEVAL BY SHAPE
+    found = Retrieval_by_shape(npimgs, classes, ['Shorts'])
+    visualize_retrieval(found, len(found))
+
+    # RETRIEVAL COMBINED
+    found = Retrieval_combined(npimgs, class_labels, nplabels, ['Shorts'], ['Green'])
+    visualize_retrieval(found, len(found))
+
